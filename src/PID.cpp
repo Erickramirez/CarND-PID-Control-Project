f6771c6314813c5a30c21efa6b39341926102dd8
Erick Ramirez <erickramireztebalan@gmail.com>
2017-06-24 21:52:36 -0600

    First load

---
+++ src/PID.cpp
@@ -0,0 +1,39 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
+}
+
+void PID::UpdateError(double cte) {
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += cte;
+}
+
+double PID::TotalError() {
+    double steer = -Kp * p_error -Kd * d_error - Ki * i_error;
+    if (steer<-1.0){
+        steer = -1.0;
+    } else if (steer>1.0) {
+        steer = 1.0;
+    }
+
+    return steer;
+}
+
